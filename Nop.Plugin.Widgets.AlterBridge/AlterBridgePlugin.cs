c75b569c9891297e1bc11282f2094a92c48e3b06
Nayef Al Mahbub <53393197+nayef-shuvo@users.noreply.github.com>
2024-04-04 20:35:50 +0600

    Add files via upload

---
+++ Nop.Plugin.Widgets.AlterBridge/AlterBridgePlugin.cs
@@ -0,0 +1,30 @@
+﻿using Nop.Plugin.Widgets.AlterBridge.Components;
+using Nop.Services.Cms;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Infrastructure;
+
+namespace Nop.Plugin.Widgets.AlterBridge;
+
+public class AlterBridge : BasePlugin, IWidgetPlugin
+{
+    public bool HideInWidgetList => false;
+
+    public Type GetWidgetViewComponent(string widgetZone)
+    {
+        return typeof(WidgetsAlterBridgeViewComponent);
+    }
+
+    public Task<IList<string>> GetWidgetZonesAsync()
+    {
+        return Task.FromResult<IList<string>>( [PublicWidgetZones.HomepageBeforeProducts, PublicWidgetZones.HomepageBottom] );
+    }
+
+    public override async Task InstallAsync()
+    {
+        await base.InstallAsync();
+    }
+    public override async Task UninstallAsync()
+    {
+        await base.UninstallAsync();
+    }
+}
---
+++ Nop.Plugin.Widgets.AlterBridge/Components/WidgetsAlterBridgeViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using DocumentFormat.OpenXml.Spreadsheet;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Plugins;
+using Nop.Web.Framework.Components;
+
+namespace Nop.Plugin.Widgets.AlterBridge.Components;
+public class WidgetsAlterBridgeViewComponent : NopViewComponent
+{
+    public IViewComponentResult Invoke(string widgetZone, object additionalData)
+    {
+        return View("~/Plugins/Widgets.AlterBridge/Views/Default.cshtml");
+    }
+}
